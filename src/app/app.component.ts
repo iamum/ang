645a02cdc8d3371785e73e51a4b163c484af7b30
Reinkaos <ds2lvg@gmail.com>
2017-12-09 16:18:01 +0900

    init angualr2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { User } from './user'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  userList:Array<User> = [];
+  userName:string ="";
+  userAge:number = 0;
+
+  constructor(){
+  }
+  addUser():void{
+    var user:User = new User();
+    user.userName = this.userName;
+    user.userAge = this.userAge;
+    this.userList.push(user);
+  }
+  test() : void{
+  var user1:User = new User();
+    this.userList.push(user1);
+    user1.userName = "장유미";
+    user1.userAge = 3;
+    user1 = new User();
+    user1.userName = "장유미1";
+    user1.userAge = 23;
+    this.userList.push(user1);
+  }
 }
